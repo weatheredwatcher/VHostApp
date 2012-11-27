30376633783a6ac9c5254a5d2288e0036ab95b91
David Duggins <weatheredwatcher@gmail.com>
2012-11-27 10:29:54 -0500

    added more base functionality and modified the README

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ VHostApp
 
 This is an application for creating vhost entries.  It takes a domain name for it main argument and then creates a vhost file as well as an entry in the hosts file for the vhost domain
 
-== Version 0.1 alpha ChangeLog 11/26/2012
+###Version 0.1 alpha ChangeLog 11/27/2012###
 
 - added the main class
 - added a main function to run the app
-- created a spot for porcessing the argv.
-
+- created code for porcessing the argv.
+- created the vhost block
+- created the hosts line
+- added file support to create new 
--- VHostApp.py
+++ VHostApp.py
@@ -1,23 +1,63 @@
-# VHostApp.py
-#
-# Application to manage Virtual Hosts
-#
-#
+
+"""Virtual Host App
+
+Creates the hosts file and vhost files 
+Usage: python VHostApp.py [project domain]
+
+Options:
+  -p ..., --project=...   use specified project/domainS
+  -h, --help              show this help
+  -d                      show debugging information while parsing
+
+
+"""
+
+__author__ = "David Duggins (weatheredwatcher@gmail.com)"
+__version__ = "$Revision: 0.1 $"
+__date__ = "$Date: 2012/09/22 21:57:19 $"
+__copyright__ = "Copyright (c) 2012 David Duggins"
+__license__ = "GPL3"
+
+
+import sys
+import getopt
+
 
 
 class VHostApp():
-
+    """This is the main class file for the VHost App"""
     def __init__(self, project):
         self.project = project
         # here we handle the command line input and test it for validity,
         # if there is an issue, we end the program and spit out errors.
     def print_name(self):
-        print "<VirtualHost *:80>\n\t\t<IfModule mod_expires.c>\n\t\t\t\tExpiresActive Off\n\t\t</IfModule>\n\t\t##SSLEngine off\n\t\tServerAlias %s\n\n\t\tDocumentRoot /var/www/html/%s\n\t\tErrorLog logs/%s-error_log\n\t\tCustomLog logs/%s-access_log combined\n</VirtualHost>" % (self.project, self.project, self.project, self.project)
+        print """
+        <VirtualHost *:80>
+            ServerAlias %s
+            DocumentRoot /var/www/html/%s
+            ErrorLog logs/%s-error_log
+            CustomLog logs/%s-access_log combined
+        </VirtualHost>""" % (self.project, self.project, self.project, self.project)
+def usage():
+        print __doc__
+        
+def main(argv):
+    
+    try:
+        opts, args = getopt.getopt(argv, "hp:", ["help", "project="])
+    except getopt.GetoptError:           
+        usage()                          
+        sys.exit(2)  
+    for opt, arg in opts:                
+        if opt in ("-h", "--help"):      
+            usage()                     
+            sys.exit()                                
+        elif opt in ("-p", "--project"): 
+            project = arg
+    
+    me = VHostApp(project)
+    me.print_name()
+    
+    
 
-
-def main():
-
-    c = VHostApp('myapp')
-    c.print_name()
-
-main()
+main(sys.argv[1:])
